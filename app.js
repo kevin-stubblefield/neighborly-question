4002185f736199c68af254ee8435daeadae0b0f2
Kevin Stubblefield <kevins12.92@gmail.com>
2017-09-23 20:04:13 -0700

    Implement Gale-Shapley variation

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-function Entity(name) {
+function Buyer(name, desiredSellers) {
 
     var currentCandidate = 0;
 
     this.name = name;
-    this.partner = null;
+    this.partners = [];
+    this.desiredSellers = desiredSellers;
     this.candidates = [];
 
     this.rank = function(entity) {
@@ -24,23 +25,43 @@ function Entity(name) {
 
     this.partnerUp = function(entity) {
         if (entity.partner) {
-            entity.partner.partner = null;
+            var index = entity.partner.partners.indexOf(entity);
+            entity.partner.partners.splice(index, 1);
         }
         entity.partner = this;
-        if (this.partner) {
-            this.partner.partner = null;
+        if (this.partners.length >= desiredSellers) {
+            this.partners.shift();
         }
-        this.partner = entity;
+        this.partners.push(entity);
     }
 }
 
-function stableMatching(proposors) {
+function Seller(name) {
+    var currentCandidate = 0;
+    
+    this.name = name;
+    this.partner = null;
+    this.candidates = [];
+
+    this.rank = function(entity) {
+        for (var i = 0; i < this.candidates.length; i++) {
+            if (this.candidates[i] === entity) return i;
+        }
+        return this.candidates.length + 1;
+    }
+
+    this.prefers = function(entity) {
+        return this.rank(entity) < this.rank(this.partner);
+    }
+}
+
+function galeShapley(proposors) {
     var done = false;
     while (!done) {
         done = true;
         for (var i = 0; i < proposors.length; i++) {
             var proposor = proposors[i];
-            if (!proposor.partner) {
+            if (proposor.partners.length < proposor.desiredSellers) {
                 done = false;
                 var acceptor = proposor.next();
                 if (!acceptor.partner || acceptor.prefers(proposor)) {
@@ -52,32 +73,52 @@ function stableMatching(proposors) {
 }
 
 function run() {
-    var bingley = new Entity('Bingley');
-    var wickham = new Entity('Wickham');
-    var collins = new Entity('Collins');
-    var darcy = new Entity('Darcy');
-    var charlotte = new Entity('Charlotte');
-    var elizabeth = new Entity('Elizabeth');
-    var jane = new Entity('Jane');
-    var lydia = new Entity('Lydia');
+    var walmart = new Buyer('Walmart', 3);
+    var target = new Buyer('Target', 2);
+    var zumiez = new Buyer('Zumiez', 1);
+    var nordstrom = new Buyer('Nordstrom', 2);
+    var kohls = new Buyer('Kohl\'s', 1);
 
-    bingley.candidates = [jane, elizabeth, lydia, charlotte];
-    wickham.candidates = [lydia, jane, elizabeth, charlotte];
-    darcy.candidates = [elizabeth, jane, charlotte, lydia];
-    collins.candidates = [jane, elizabeth, lydia, charlotte];
-    charlotte.candidates = [bingley, darcy, collins, wickham];
-    elizabeth.candidates = [wickham, darcy, bingley, collins];
-    jane.candidates = [bingley, wickham, darcy, collins];
-    lydia.candidates = [bingley, wickham, darcy, collins];
+    var mossimo = new Seller('Mossimo');
+    var fadedGlory = new Seller('Faded Glory');
+    var tommyHilfiger = new Seller('Tommy Hilfiger');
+    var hanes = new Seller('Hanes');
+    var levis = new Seller('Levi\'s');
+    var calvinKlein = new Seller('Calvin Klein');
+    var adidas = new Seller('Adidas');
+    var vans = new Seller('Vans');
+    var docker = new Seller('Docker');
 
-    var guys = [bingley, darcy, collins, wickham];
+    walmart.candidates = [hanes, docker, levis, calvinKlein, tommyHilfiger, mossimo, adidas, vans, fadedGlory];
+    target.candidates = [mossimo, hanes, calvinKlein, docker, tommyHilfiger, adidas, levis, fadedGlory, vans];
+    zumiez.candidates = [vans, calvinKlein, levis, hanes, tommyHilfiger, fadedGlory, docker, mossimo, adidas];
+    nordstrom.candidates = [calvinKlein, levis, tommyHilfiger, hanes, mossimo, fadedGlory, adidas, vans, docker];
+    kohls.candidates = [hanes, levis, docker, calvinKlein, tommyHilfiger, mossimo, adidas, vans, fadedGlory];
+
+    mossimo.candidates = [kohls, target, walmart, nordstrom, zumiez];
+    fadedGlory.candidates = [walmart, target, zumiez, nordstrom, kohls];
+    tommyHilfiger.candidates = [zumiez, nordstrom, kohls, target, walmart];
+    hanes.candidates = [target, walmart, nordstrom, kohls, zumiez];
+    levis.candidates = [target, nordstrom, kohls, zumiez, walmart];
+    calvinKlein.candidates = [nordstrom, kohls, target, walmart, zumiez];
+    adidas.candidates = [target, kohls, nordstrom, walmart, zumiez];
+    vans.candidates = [nordstrom, zumiez, target, walmart, kohls];
+    docker.candidates = [kohls, zumiez, nordstrom, target, walmart];
+
+    var buyers = [walmart, target, zumiez, nordstrom, kohls];
     
-    stableMatching(guys);
+    galeShapley(buyers);
 
-    for (var i = 0; i < guys.length; i++) {
-        console.log('%s is engaged to %s', guys[i].name, guys[i].partner.name);
+    for (var i = 0; i < buyers.length; i++) {
+        var output = buyers[i].name + ' is partnered with ';
+        for (var j = 0; j < buyers[i].partners.length; j++) {
+            output += buyers[i].partners[j].name;
+            if (j < buyers[i].partners.length - 1) {
+                output += ', '
+            }
+        }
+        console.log(output);
     }
 }
 
 run();
-
